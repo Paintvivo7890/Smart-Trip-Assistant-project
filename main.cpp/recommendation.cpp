9eade4db9b294f6c1121a2723383165c325e68b0
thathanathip679 <thanathip.nan@gmail.com>
2026-02-27 20:34:02 +0700

    edit2_thanathip

--- main.cpp/recommendation.cpp
+++ main.cpp/recommendation.cpp
@@ -1,35 +1,64 @@
 #include "recommendation.h"
 
-// เช็คว่า tripstyleของผู้ใช้ ตรงกับ styleของสถานที่นั้นรึเปล่า 
-//อันนี้กรณีที่tripstyleของสถานที่นั้นเป็นvector คือ สถานที่นั้นมีหลายstyle
-
-
 // แนะนำสถานที่ ตาม จังหวัดที่เลือก,สไตล์การเที่ยว
-// TS = TripStyle 
-vector<place> Recommendation_place(vector<place> &allplace, string ){
-    vector<place> Place_accordingto_condition;
-    
+// TS = TripStyle
+vector<place> Screening_provice(vector<place> &allplace, int pv){
+    vector<place> province_accordingto_condition;
+    string PV;
+    if(pv == 1) PV = "Chiang Mai";
+    if(pv == 2) PV = "Chiang Rai";
+    if(pv == 3) PV = "Mae Hong Son";
+    if(pv == 4) PV = "Lamphun";
+    if(pv == 5) PV = "Lampang";
+    if(pv == 6) PV = "Phayao";
+    if(pv == 7) PV = "Phrae";
+    if(pv == 8) PV = "Nan";
+    if(pv == 9) PV = "Uttaradit";
+    if(pv == 10) PV = "Tak";
+    if(pv == 11) PV = "Sukhothai";
+    if(pv == 12) PV = "Phitsanulok";
+    if(pv == 12) PV = "Kamphaeng Phet";
+    if(pv == 14) PV = "Phetchabun";
+    if(pv == 15) PV = "Phichit";
+    if(pv == 16) PV = "Nakhon Sawan";
+    if(pv == 17) PV = "Uthai Thani";
+
     for(int i = 0; i < allplace.size(); i++){
-        if(TS == allplace.type[i]){
-            Place_accordingto_condition.push_back(province[i]);
+        place p = allplace[i];
+        if(PV == p.province[i]){
+            province_accordingto_condition.push_back(allplace[i]);
+        }
+    }
+    return province_accordingto_condition;
+}
+
+vector<place> Recommendation_place(vector<place> &pac, int ts ){
+    vector<place> Place_accordingto_condition;
+    string TS;
+    if(ts == 1) TS = "Culture";
+    if(ts == 2) TS = "Adventure";
+    if(ts == 3) TS = "Natural";
+    if(ts == 4) TS = "Photo";
+    if(ts == 5) TS = "Cafe";
+
+    for(int i = 0; i < pac.size(); i++){
+        place p = pac[i];
+        if(TS == p.type[i]){
+            Place_accordingto_condition.push_back(pac[i]);
         }
     }
     return Place_accordingto_condition;
 }
 
 
-// โชว์สถานที่ ที่ตรงกับเงื่อนไขจากฟังก์ชัน Recommendation_place
+// โชว์สถานที่ ที่ตรงกับเงื่อนไขจากฟังก์ชัน Recommendation_place 
 void Show_Place(vector<place> &P){
-    cout << "===================="
+    cout << "====================";
     for(int i = 0; i < P.size(); i++){
-        cout << "Name of place " << "[" << i+1 << "] : " << P.name << endl;
-        cout << "Total cost (Mininum) : " << P.cost << endl;
-        cout << "The style of place : ";
-        for(int j = 0, j < P.tripstyle.size(), i++){
-            if(j < P.tripstyle.size() - 1){
-                cout << P.tripstyle[j]; << " , ";
-            }else cout << P.tripstyle[j] << endl;
-        }
+        place p = P[i];
+        cout << "Name of place " << "[" << i+1 << "] : " << p.name << endl;
+        cout << "District : " << p.district << endl;
+        cout << "Entrance Fee : " << p.price << endl;
         cout << "====================" << endl;
     }
 }
